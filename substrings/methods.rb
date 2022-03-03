f8f0a4547fd918dcb7f195359871b9604ee76ed7
Kevin Sander <kevintsander@gmail.com>
2022-03-03 18:54:52 -0500

    added comments

--- substrings/methods.rb
+++ substrings/methods.rb
@@ -1,6 +1,10 @@
 def substrings(word_str, dictionary)
+    # split the words into array
     word_arr = word_str.split(' ')
+
+    # count dictionary words that are included in the input string
     dictionary.reduce(Hash.new(0)) do |counts, dict_word|
+        #check each word in input string separately
         word_arr.each do |word|
             if (word.downcase.include?(dict_word.downcase))
                 counts[dict_word] += 1
