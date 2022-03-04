d12639eb1dabbd151d175009850ce3a515e61e0d
Kevin Sander <kevintsander@gmail.com>
2022-03-04 13:56:05 -0500

    added bubble_sort method

---
+++ bubble-sort/methods.rb
@@ -0,0 +1,24 @@
+def bubble_sort(numbers)
+    swapped = nil
+    sortednumbers = numbers
+
+    n = 0
+    # check until sorted
+    until swapped = false do
+        m = 0
+
+        #check until last index - iterations (n)
+        while m < sortednumbers.size - 1 - n do
+            puts "size: #{sortednumbers.size} m: #{m} n: #{n}"
+            if sortednumbers[m + 1] < sortednumbers[m]
+                sortednumbers.insert(m, sortednumbers.delete_at(m + 1))
+                swapped = true
+            end
+            m += 1
+        end
+
+        n += 1
+    end
+    sortednumbers
+
+end
