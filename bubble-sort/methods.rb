664a3cf78e8aeb64ce5626075437729484972fa1
Kevin Sander <kevintsander@gmail.com>
2022-03-04 14:11:27 -0500

    fixed bug in loop

--- bubble-sort/methods.rb
+++ bubble-sort/methods.rb
@@ -4,9 +4,10 @@ def bubble_sort(numbers)
 
     n = 0
     # check until sorted
-    until swapped = false do
+    until swapped == false do
         m = 0
 
+        swapped = false
         #check until last index - iterations (n)
         while m < sortednumbers.size - 1 - n do
             if sortednumbers[m + 1] < sortednumbers[m]
@@ -15,7 +16,6 @@ def bubble_sort(numbers)
             end
             m += 1
         end
-
         n += 1
     end
     sortednumbers
