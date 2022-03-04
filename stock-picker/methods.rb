1dc81d7caabbbf4391be16e8cdc69c51006552a3
Kevin Sander <kevintsander@gmail.com>
2022-03-04 12:17:06 -0500

    simplified logic

--- stock-picker/methods.rb
+++ stock-picker/methods.rb
@@ -3,10 +3,9 @@ def stock_picker(stock_days)
     best = []
     #check each buy day (never last day)
     stock_days[0...-1].each_with_index do |buy_price, buy_day|
-        sell_days = stock_days[buy_day + 1..]
         
-        #check each sell day
-        sell_days.each_with_index do |sell_price, sell_day|
+        #check each sell day (after buy day)
+        stock_days[buy_day + 1..].each_with_index do |sell_price, sell_day|
 
             #test if selling on this day is best
             if (sell_price - buy_price > max)
