72650a6de35d94bcb5fcdf3adc3d1198a43d0bf5
Kevin Sander <kevintsander@gmail.com>
2022-03-29 21:19:36 -0400

    separated win conditions into own methods

--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -5,7 +5,6 @@ class TicTacToeGame
     @player2 = player2
 
     @board = Array.new(3) { Array.new(3) }
-    p @board
   end
 
   private
@@ -22,64 +21,34 @@ class TicTacToeGame
     @board[row][column] == nil ? true : false
   end
 
-  def win?
-    win = false
-    win = @board.any? do |row|
+  def won_row?
+    return @board.any? do |row|
       row.uniq.count == 1 && row.none?(&:nil?)
     end
-    if win == true
-      return true
-    end
-
-    #transpose and check columns
-    win = @board.transpose.any? do |col|
-      col.uniq.count == 1 && col.none?(&:nil?)
-    end
-    if win == true
-      return true
-    end
-
-    #check diagonals
-    left_i = 0
-    left_diag = []
-    while left_i < @board.size
-      left_diag.push(@board[left_i][left_i])
-      left_i += 1
-    end
-    win = left_diag.uniq.count == 1 && left_diag.none?(&:nil?)
-    if win == true
-      return true
-    end
-
-    right_i = @board.size - 1
-    right_diag = []
-    while right_i >= 0
-      right_diag.push(@board[right_i][right_i])
-      right_i -= 1
-    end
-    win = right_diag.uniq.count == 1 && right_diag.none?(&:nil?)
-    if win == true
-      return true
-    end
-
-    false
   end
 
-  def display_board
-    text = ''
-    @board.each_with_index do |row, row_i|
-      rowtext = ''
-      row.each_with_index do |item, col_i|
-        puts "item: #{item}"
-        rowtext += item.nil? ? '   ' : " #{item} "
-        puts "col_i: #{col_i} row size: #{row.size}"
-        col_i != row.size - 1 && rowtext += '|'
-      end
-      text += rowtext
-      row_i != @board.size - 1 && text += "\n---+---+---\n"
+  def won_column?
+    return @board.transpose.any? do |col|
+      col.uniq.count == 1 && col.none?(&:nil?)
     end
+  end
 
-    puts text
+  def won_diagonal?
+    i = 0
+    left_diag = []
+    right_diag = []
+    size = @board.size
+    while i < size
+      left_diag.push(@board[i][i])
+      right_diag.push(@board[size - i  - 1][size - i - 1])
+      i += 1
+    end
+    return (left_diag.uniq.count == 1 && left_diag.none?(&:nil?)) ||
+           (right_diag.uniq.count == 1 && right_diag.none?(&:nil?))
+  end
+
+  def won?
+    won_row? || won_column? || won_diagonal?
   end
 
   public
@@ -104,13 +73,13 @@ class TicTacToeGame
 
 
   def play_game
-    while !win?
+    while !won?
       play_turn(@player1)
-      if win?
+      if won?
         return win_message(@player1)
       end
       play_turn(@player2)
-      if win?
+      if won?
         return win_message(@player2)
       end
     end
@@ -119,6 +88,23 @@ class TicTacToeGame
   def update_board(player, row, column)
     @board[row][column] = player
     display_board
+  end
+
+  def display_board
+    text = ''
+    @board.each_with_index do |row, row_i|
+      rowtext = ''
+      row.each_with_index do |item, col_i|
+        puts "item: #{item}"
+        rowtext += item.nil? ? '   ' : " #{item} "
+        puts "col_i: #{col_i} row size: #{row.size}"
+        col_i != row.size - 1 && rowtext += '|'
+      end
+      text += rowtext
+      row_i != @board.size - 1 && text += "\n---+---+---\n"
+    end
+
+    puts text
   end
 end
 
