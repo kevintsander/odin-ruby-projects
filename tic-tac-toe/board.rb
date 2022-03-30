0c9b7a171c7118314220f2928a77fb254d946a4c
Kevin Sander <kevintsander@gmail.com>
2022-03-29 21:35:34 -0400

    separated board into own class

---
+++ tic-tac-toe/board.rb
@@ -0,0 +1,65 @@
+class TicTacToeBoard
+    def initialize
+        @board = Array.new(3) { Array.new(3) }
+    end
+
+    private
+
+    def won_row?
+        return @board.any? do |row|
+            row.uniq.count == 1 && row.none?(&:nil?)
+        end
+    end
+    
+    def won_column?
+        return @board.transpose.any? do |col|
+            col.uniq.count == 1 && col.none?(&:nil?)
+        end
+    end
+    
+    def won_diagonal?
+        i = 0
+        left_diag = []
+        right_diag = []
+        size = @board.size
+        while i < size
+          left_diag.push(@board[i][i])
+          right_diag.push(@board[size - i  - 1][size - i - 1])
+          i += 1
+        end
+        return (left_diag.uniq.count == 1 && left_diag.none?(&:nil?)) ||
+               (right_diag.uniq.count == 1 && right_diag.none?(&:nil?))
+    end
+
+    public
+
+    def location_open?(row, column)
+        @board[row][column] == nil ? true : false
+    end
+
+    def update_board(player, row, column)
+        @board[row][column] = player
+        display_board
+    end
+
+    def display_board
+        text = ''
+        @board.each_with_index do |row, row_i|
+          rowtext = ''
+          row.each_with_index do |item, col_i|
+            puts "item: #{item}"
+            rowtext += item.nil? ? '   ' : " #{item} "
+            puts "col_i: #{col_i} row size: #{row.size}"
+            col_i != row.size - 1 && rowtext += '|'
+          end
+          text += rowtext
+          row_i != @board.size - 1 && text += "\n---+---+---\n"
+        end
+    
+        puts text
+    end
+    
+    def won?
+        won_row? || won_column? || won_diagonal?
+    end
+end
--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'board.rb'
+
 # Represents a tic-tac-toe game
 class TicTacToeGame
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-
-    @board = Array.new(3) { Array.new(3) }
+    @board = TicTacToeBoard.new
   end
 
   private
@@ -21,36 +25,6 @@ class TicTacToeGame
     @board[row][column] == nil ? true : false
   end
 
-  def won_row?
-    return @board.any? do |row|
-      row.uniq.count == 1 && row.none?(&:nil?)
-    end
-  end
-
-  def won_column?
-    return @board.transpose.any? do |col|
-      col.uniq.count == 1 && col.none?(&:nil?)
-    end
-  end
-
-  def won_diagonal?
-    i = 0
-    left_diag = []
-    right_diag = []
-    size = @board.size
-    while i < size
-      left_diag.push(@board[i][i])
-      right_diag.push(@board[size - i  - 1][size - i - 1])
-      i += 1
-    end
-    return (left_diag.uniq.count == 1 && left_diag.none?(&:nil?)) ||
-           (right_diag.uniq.count == 1 && right_diag.none?(&:nil?))
-  end
-
-  def won?
-    won_row? || won_column? || won_diagonal?
-  end
-
   public
 
   def play_turn(player)
@@ -63,49 +37,34 @@ class TicTacToeGame
       row = gets.chomp.to_i - 1
       puts "Column:"
       column = gets.chomp.to_i - 1
-      position_open = location_open?(row, column)
+      position_open = @board.location_open?(row, column)
       if position_open == false
         space_taken_message
       end
     end
-    update_board(player, row, column)
+    @board.update_board(player, row, column)
   end
 
 
   def play_game
-    while !won?
+    while !@board.won?
       play_turn(@player1)
-      if won?
+      if @board.won?
         return win_message(@player1)
       end
       play_turn(@player2)
-      if won?
+      if @board.won?
         return win_message(@player2)
       end
     end
   end
 
   def update_board(player, row, column)
-    @board[row][column] = player
+    @board.board[row][column] = player
     display_board
   end
 
-  def display_board
-    text = ''
-    @board.each_with_index do |row, row_i|
-      rowtext = ''
-      row.each_with_index do |item, col_i|
-        puts "item: #{item}"
-        rowtext += item.nil? ? '   ' : " #{item} "
-        puts "col_i: #{col_i} row size: #{row.size}"
-        col_i != row.size - 1 && rowtext += '|'
-      end
-      text += rowtext
-      row_i != @board.size - 1 && text += "\n---+---+---\n"
-    end
-
-    puts text
-  end
+  
 end
 
 game = TicTacToeGame.new('O', 'X')
