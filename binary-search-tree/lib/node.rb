f6a3542572151b0db5cc536f3142ffa87b967b11
Kevin Sander <kevintsander@gmail.com>
2022-07-11 21:30:02 -0400

    added Comparable

--- binary-search-tree/lib/node.rb
+++ binary-search-tree/lib/node.rb
@@ -2,10 +2,15 @@
 
 # Represents a tree node
 class Node
+  include Comparable
   attr_accessor :left, :right, :data
   def initialize(data)
     @data = data 
     @left = nil
     @right = nil
+  end
+
+  def <=>(other)
+    @data <=> other.data
   end
 end
