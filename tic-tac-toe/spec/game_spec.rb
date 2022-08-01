431d4fb795cf833aaa36e96c60cc1d8710ef191c
Kevin Sander <kevintsander@gmail.com>
2022-08-01 10:55:50 -0400

    added tests

--- tic-tac-toe/spec/game_spec.rb
+++ tic-tac-toe/spec/game_spec.rb
@@ -3,12 +3,13 @@
 require './lib/game'
 
 describe TicTacToeGame do
+  let(:board) { instance_double(TicTacToeBoard) }
+  subject(:game) { described_class.new('X', 'O', board) }
+
   describe '#play_turn' do
     context 'user plays an open space' do
-      let(:board) { instance_double(TicTacToeBoard) }
-      subject(:game) { described_class.new('X', 'O', board) }
-
       before do
+        player = 'X'
         allow(game).to receive(:puts)
         allow(game).to receive(:gets).and_return('1', '1')
         allow(board).to receive(:location_open?).with(0, 0).and_return(true)
@@ -23,7 +24,30 @@ describe TicTacToeGame do
 
       it 'sends update_board to board' do
         player = 'X'
-        expect(board).to receive(:update_board).with('X', 0, 0)
+        expect(board).to receive(:update_board).with(player, 0, 0)
+        game.play_turn(player)
+      end
+    end
+
+    context 'user plays a taken space once before playing an open space' do
+      before do
+        player = 'X'
+        allow(game).to receive(:puts)
+        allow(game).to receive(:gets).and_return('1', '1', '2', '2')
+        allow(board).to receive(:location_open?).with(0, 0).and_return(false)
+        allow(board).to receive(:location_open?).with(1, 1).and_return(true)
+        allow(board).to receive(:update_board)
+      end
+
+      it 'displays an error message once' do
+        player = 'X'
+        expect(game).to receive(:space_taken_message).once
+        game.play_turn(player)
+      end
+
+      it 'sends update_board to board' do
+        player = 'X'
+        expect(board).to receive(:update_board).with(player, 1, 1)
         game.play_turn(player)
       end
     end
