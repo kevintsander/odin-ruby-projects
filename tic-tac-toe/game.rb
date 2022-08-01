d87109d8a63965bd1b983ad38c5d97fc8c19c4a2
Kevin Sander <kevintsander@gmail.com>
2022-08-01 00:21:02 -0400

    removed unused method

--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'board.rb'
+require_relative 'board'
 
 # Represents a tic-tac-toe game
 class TicTacToeGame
-
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
@@ -21,10 +20,6 @@ class TicTacToeGame
     puts "#{player} WON!!!"
   end
 
-  def location_open?(row, column)
-    @board[row][column] == nil ? true : false
-  end
-
   public
 
   def play_turn(player)
@@ -33,29 +28,23 @@ class TicTacToeGame
     row = nil
     column = nil
     while position_open == false
-      puts "Row:"
+      puts 'Row:'
       row = gets.chomp.to_i - 1
-      puts "Column:"
+      puts 'Column:'
       column = gets.chomp.to_i - 1
       position_open = @board.location_open?(row, column)
-      if position_open == false
-        space_taken_message
-      end
+      space_taken_message if position_open == false
     end
     @board.update_board(player, row, column)
   end
 
-
   def play_game
-    while !@board.won?
+    until @board.won?
       play_turn(@player1)
-      if @board.won?
-        return win_message(@player1)
-      end
+      return win_message(@player1) if @board.won?
+
       play_turn(@player2)
-      if @board.won?
-        return win_message(@player2)
-      end
+      return win_message(@player2) if @board.won?
     end
   end
 
@@ -63,8 +52,6 @@ class TicTacToeGame
     @board.board[row][column] = player
     display_board
   end
-
-  
 end
 
 game = TicTacToeGame.new('O', 'X')
