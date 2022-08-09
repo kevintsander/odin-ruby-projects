189e16c63013a8a32051ec005d6d79f583346798
Kevin Sander <kevintsander@gmail.com>
2022-08-09 13:57:25 -0400

    show board once before first turn

--- connect-four/lib/game.rb
+++ connect-four/lib/game.rb
@@ -26,6 +26,7 @@ class Game
     @current_player = player1
     loop do
       this_player = @current_player
+      board.display
       display_turn
       if board.four_in_a_row?
         winner(this_player)
